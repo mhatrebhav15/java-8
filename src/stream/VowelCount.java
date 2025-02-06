a7dad3d7896c8bdcc1e751424b9e011c84a3f839
Bhavesh Mhatre <mhatrebhav15@gmail.com>
2025-02-06 19:22:43 +0530

    Write a program to count the number of vowels in a given string.

---
+++ src/stream/VowelCount.java
@@ -0,0 +1,23 @@
+package stream;
+
+/**
+ * 
+ * Count the number of vowels in a given string
+ * 
+ */
+public class VowelCount {
+	
+    public static long countVowels(String str) {
+        return str.chars()	// Convert the string into an IntStream
+                  .mapToObj(c -> (char) c)	// Convert each character from int to Character
+                  .filter(c -> "AEIOUaeiou".indexOf(c) != -1)	// Filter out vowels
+                  .count();	// Count the remaining vowels
+    }
+
+    public static void main(String[] args) {
+        String input = "I am going to be King of the Pirates";
+        long vowelCount = countVowels(input);
+        System.out.println("Number of vowels: " + vowelCount);
+    }
+
+}
