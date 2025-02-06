71bb6786d87ef84a9bcffb5d5ea74ab68b391e13
Bhavesh Mhatre <mhatrebhav15@gmail.com>
2025-02-06 19:28:19 +0530

    Program to find distinct characters in a string

---
+++ src/stream/DistinctCharacters.java
@@ -0,0 +1,20 @@
+package stream;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DistinctCharacters {
+	
+    public static List<Character> getDistinctCharacters(String str) {
+        return str.chars()	// Convert the string into an IntStream
+                  .mapToObj(c -> (char) c)	// Convert each int value to a Character
+                  .distinct()	// Remove duplicate characters
+                  .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        String input = "Java 8 Stream API";
+        List<Character> distinctChars = getDistinctCharacters(input);
+        System.out.println("Distinct characters: " + distinctChars);
+    }
+
+}
