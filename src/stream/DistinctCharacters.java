5e672dcdad4bad61c1bea5f2363492b9be7cef6b
Bhavesh Mhatre <mhatrebhav15@gmail.com>
2025-02-06 21:55:36 +0530

    Code Formatted

--- src/stream/DistinctCharacters.java
+++ src/stream/DistinctCharacters.java
@@ -1,20 +1,26 @@
 package stream;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * 
+ * Program to find distinct characters in a string
+ * 
+ */
 public class DistinctCharacters {
-	
-    public static List<Character> getDistinctCharacters(String str) {
-        return str.chars()	// Convert the string into an IntStream
-                  .mapToObj(c -> (char) c)	// Convert each int value to a Character
-                  .distinct()	// Remove duplicate characters
-                  .collect(Collectors.toList());
-    }
 
-    public static void main(String[] args) {
-        String input = "Java 8 Stream API";
-        List<Character> distinctChars = getDistinctCharacters(input);
-        System.out.println("Distinct characters: " + distinctChars);
-    }
+	public static List<Character> getDistinctCharacters(String str) {
+		return str.chars() // Convert the string into an IntStream
+				.mapToObj(c -> (char) c) // Convert each int value to a Character
+				.distinct() // Remove duplicate characters
+				.collect(Collectors.toList());
+	}
+
+	public static void main(String[] args) {
+		String input = "Java 8 Stream API";
+		List<Character> distinctChars = getDistinctCharacters(input);
+		System.out.println("Distinct characters: " + distinctChars);
+	}
 
 }
--- src/stream/VowelCount.java
+++ src/stream/VowelCount.java
@@ -6,18 +6,18 @@ package stream;
  * 
  */
 public class VowelCount {
-	
-    public static long countVowels(String str) {
-        return str.chars()	// Convert the string into an IntStream
-                  .mapToObj(c -> (char) c)	// Convert each character from int to Character
-                  .filter(c -> "AEIOUaeiou".indexOf(c) != -1)	// Filter out vowels
-                  .count();	// Count the remaining vowels
-    }
 
-    public static void main(String[] args) {
-        String input = "I am going to be King of the Pirates";
-        long vowelCount = countVowels(input);
-        System.out.println("Number of vowels: " + vowelCount);
-    }
+	public static long countVowels(String str) {
+		return str.chars() // Convert the string into an IntStream
+				.mapToObj(c -> (char) c) // Convert each character from int to Character
+				.filter(c -> "AEIOUaeiou".indexOf(c) != -1) // Filter out vowels
+				.count(); // Count the remaining vowels
+	}
+
+	public static void main(String[] args) {
+		String input = "I am going to be King of the Pirates";
+		long vowelCount = countVowels(input);
+		System.out.println("Number of vowels: " + vowelCount);
+	}
 
 }
